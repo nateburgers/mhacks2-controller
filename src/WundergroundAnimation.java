f45498bad4197dc9d55625ebf46a998c6446ff4d
Lee Avital <leeavital@gmail.com>
2014-01-18 20:53:41 -0500

    added interface for WundergroundImage

---
+++ src/WundergroundAnimation.java
@@ -0,0 +1,19 @@
+import processing.core.PImage;
+
+/**
+ * Created by lee on 1/18/14.
+ */
+public class WundergroundAnimation {
+
+    public WundergroundAnimation(  String locString ){
+
+
+    }
+
+
+    public PImage getFrame( int i ){
+
+    }
+
+
+}
